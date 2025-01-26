227a46ce18087413f45dbb7caf40470ae3cf80ef
brijesh-giri-neu <giri.b@northeastern.edu>
2025-01-26 03:20:35 -0500

    Modulared part 1

--- src/pages/qcomps/gallery_props.tsx
+++ src/pages/qcomps/gallery_props.tsx
@@ -1,29 +1,40 @@
 import { GalleryProps } from "@/types/gallery";
+import { AvatarProps } from "@/types/avatar";
+import { ListItemProps } from "@/types/listItem";
+
+function Avatar({ person, size }: AvatarProps) {
+  return (
+    <img
+      className="avatar"
+      src={'https://i.imgur.com/' + person.imageId + '.jpg'}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
+}
+
+function ListItem({ label, content }: ListItemProps) {
+  return (
+    <li>
+      <b>{label}: </b>
+      {content.includes(",") !== false ? <b>{content.split(",").length}</b> : null} {content}
+    </li>
+  );
+}
 
 function Profile({scientist, size = 100} : GalleryProps) {
   return (
     <section className="profile">
       <h2>{scientist.name}</h2>
-      <img
-        className="avatar"
-        src={'https://i.imgur.com/' + scientist.imageId + '.jpg'}
-        alt={scientist.name}
-        width={size}
-        height={size}
-      />
+      <Avatar person={{ 
+        name: scientist.name, 
+        imageId: scientist.imageId 
+      }} size={size} />
       <ul>
-        <li>
-          <b>Profession: </b>
-          {scientist.profession}
-        </li>
-        <li>
-          <b>Awards: {scientist.awards.split(',').length} </b>
-          ({scientist.awards})
-        </li>
-        <li>
-          <b>Discovered: </b>
-          {scientist.discovery}
-        </li>
+        <ListItem label="Profession" content={scientist.profession} />
+        <ListItem label="Awards" content={scientist.awards} />
+        <ListItem label="Discovered" content={scientist.discovery} />
       </ul>
     </section>
   )
@@ -34,19 +45,19 @@ export default function Gallery() {
     <div>
       <h1>Notable Scientists</h1>
       <Profile scientist={{
-          name: 'Maria Skłodowska-Curie',
-          imageId: 'szV5sdG',
-          profession: 'physicist and chemist',
-          awards: 'Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal',
-          discovery: 'polonium (element)'
+        name: 'Maria Skłodowska-Curie',
+        imageId: 'szV5sdG',
+        profession: 'physicist and chemist',
+        awards: 'Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal',
+        discovery: 'polonium (element)'
       }} size={70} />
 
       <Profile scientist={{
-          name: 'Katsuko Saruhashi',
-          imageId: 'YfeOqp2s',
-          profession: 'geochemist',
-          awards: 'Miyake Prize for geochemistry, Tanaka Prize',
-          discovery: 'a method for measuring carbon dioxide in seawater'
+        name: 'Katsuko Saruhashi',
+        imageId: 'YfeOqp2s',
+        profession: 'geochemist',
+        awards: 'Miyake Prize for geochemistry, Tanaka Prize',
+        discovery: 'a method for measuring carbon dioxide in seawater'
       }} size={70} />
     </div>
   );
